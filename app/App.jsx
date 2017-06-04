4f9fa8632cce2ec904b4d3210a462c84d0dc3a51
Ayoub Nejjari <raysoflee07@gmail.com>
2017-06-04 16:13:27 +0000

    creating project structure and server

---
+++ app/App.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import {Route, Router, IndexRoute, hashHistory} from 'react-router';
+
+class App extends Component {
+  render() {
+    return (
+      <h1>BoilerPlate</h1>
+    );
+  }
+}
+
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+	entry: './app/app.js',
+	output: {
+		path: __dirname,
+		filename: 'public/app.js'
+	},
+	resolve: {
+    modules:[__dirname, 'node_modules'],
+    alias: {
+      Main:'app/components/Main.jsx',
+      Nav:'app/components/Nav.jsx'
+    },
+    extensions: ['*','.js', '.jsx']
+  },
+	module: {
+		loaders: [
+			{
+			loader:'babel-loader',
+			test: /\.jsx?$/,
+			query:{
+				presets:  ['react', 'es2015', 'stage-0']
+			},
+			exclude: /(node_modules|bower_components)/
+			}
+		]
+	}
+}
