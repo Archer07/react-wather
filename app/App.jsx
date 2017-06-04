e1bf9a2b9ebf905cf0331d97fa540d7f16fba1a8
Ayoub Nejjari <raysoflee07@gmail.com>
2017-06-04 17:09:34 +0000

    including first router from react

--- app/App.jsx
+++ app/App.jsx
@@ -1,11 +1,16 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import {Route, Router, IndexRoute, hashHistory} from 'react-router';
+import Main from 'Main';
 
 class App extends Component {
   render() {
     return (
-      <h1>BoilerPlate</h1>
+      <Router history={hashHistory}>
+        <Route path="/" component={Main}>
+
+        </Route>
+      </Router>
     );
   }
 }
---
+++ app/components/Main.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class Main extends Component {
+  render() {
+    return (
+      <h2>Main Component</h2>
+    );
+  }
+}
+
+
+export default Main
