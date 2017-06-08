0493c8ee6f4119fa1d4eabc070fe9363b334956c
Ayoub Nejjari <raysoflee07@gmail.com>
2017-06-08 17:16:15 +0000

    data flow between Weather and WeatherForm

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -3,11 +3,14 @@ import WeatherMsg from 'WeatherMsg';
 import WeatherForm from 'WeatherForm';
 
 class Weather extends Component {
+  handleSearch (city) {
+    alert(city);
+  }
   render() {
     return (
       <div>
       <h2>Get The Weather:</h2>
-      <WeatherForm/>
+      <WeatherForm onSearch={this.handleSearch}/>
       <WeatherMsg />
       </div>
     );
--- app/components/WeatherForm.jsx
+++ app/components/WeatherForm.jsx
@@ -4,20 +4,26 @@ class WeatherForm extends Component {
   onSubmit(e) {
     e.preventDefault();
     let city = this.refs.inputEmail.value.trim();
-    let appid = 'b519e4388d6d14e4951410748a62beca';
-    $.ajax({
-      url:'http://api.openweathermap.org/data/2.5/weather?q='+city+'&units=metric&appid='+appid,
-      dataType:'json',
-      cache:false,
-      success: function(data) {
-        console.log(data);
-        alert(data.main.temp);
-      }.bind(this),
-      error: function(xhr, status, err) {
-        //this.setState({username:null});
-        alert(err);
-      }.bind(this)
-    });
+
+    if (city.length > 0) {
+      this.refs.inputEmail.value = '';
+      this.props.onSearch(city);
+    }
+
+    // let appid = 'b519e4388d6d14e4951410748a62beca';
+    // $.ajax({
+    //   url:'http://api.openweathermap.org/data/2.5/weather?q='+city+'&units=metric&appid='+appid,
+    //   dataType:'json',
+    //   cache:false,
+    //   success: function(data) {
+    //     console.log(data);
+    //     alert(data.main.temp);
+    //   }.bind(this),
+    //   error: function(xhr, status, err) {
+    //     //this.setState({username:null});
+    //     alert(err);
+    //   }.bind(this)
+    // });
   }
   render() {
     return (
