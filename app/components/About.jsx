d886f6b43125abf1068b99d9eff6f5b04b0413ef
Ayoub Nejjari <raysoflee07@gmail.com>
2017-06-09 12:04:13 +0000

    changing About component to a stateless functional component

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,13 +1,24 @@
 import React, {Component} from 'react';
 
-class About extends Component {
-  render() {
-    return (
-      <div>
-        <h1> About Page</h1>
-      </div>
-    );
-  }
+// class About extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <h1> About Page</h1>
+//       </div>
+//     );
+//   }
+// }
+
+// stateless functional Component
+
+let About = (props) => {
+  return (
+    <div>
+      <h1> About Page</h1>
+      <p>This is the about page from Master Branch</p>
+    </div>
+  );
 }
 
 
