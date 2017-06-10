393fbc1a92b9902536f45a9941810e1edbe40473
Ayoub Nejjari <raysoflee07@gmail.com>
2017-06-10 01:29:32 +0000

    adding start script for heroku

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ let app = express();
 const PORT = process.env.PORT || 3000;
 
 
-// check if the protocol used by the client is http, not https. Forward it to http if it's not
+//check if the protocol used by the client is http, not https. Forward it to http if it's not
+// because openWeathermap operates only on http
 app.use((req, res, next) => {
 		if (req.headers['x-forwarded-proto'] === 'http') {
 			next();
