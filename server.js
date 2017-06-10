fd824ef3b3b71724bcaf26de8cd564cee18e51eb
Ayoub Nejjari <raysoflee07@gmail.com>
2017-06-10 01:46:29 +0000

    update middlware for localhost

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ const PORT = process.env.PORT || 3000;
 //check if the protocol used by the client is http, not https. Forward it to http if it's not
 // because openWeathermap operates only on http
 app.use((req, res, next) => {
-		if (req.headers['x-forwarded-proto'] === 'http') {
-			next();
-		} else {
+		if (req.headers['x-forwarded-proto'] === 'https') {
 			res.redirect('http://' + req.hostname + req.url);
+		} else {
+			next();
 		}
 });
 
